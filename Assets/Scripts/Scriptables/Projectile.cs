f7053d62d3004bc84fe3061de5610ce7a6de2413
JonathanLi13090 <65463483+JonathanLi13090@users.noreply.github.com>
2020-06-06 12:27:23 -0700

    day1

---
+++ Assets/Scripts/Scriptables/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile", menuName = "CustomObject/Projectile")]
+public class Projectile : ScriptableObject
+{
+    public int damage;
+    public float speed;
+    public float size;
+
+}
